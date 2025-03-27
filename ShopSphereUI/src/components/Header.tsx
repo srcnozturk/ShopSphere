e2f60ebcfc9efe87b74c00f5e1d846b81e3e00b4
srcnozturk <sercan_ozturk59@hotmail.com>
2025-03-27 19:02:47 +0300

    Refactor Header component and update imports
    Updated import statements to include additional components from
    @mui/material and @mui/icons-material, and added NavLink from
    react-router. Defined new constants `links` and `navStyles` for
    navigation links and their styles. Modified `Header` component's
    JSX structure: added `Box` and `Stack` components for layout,
    included `IconButton` with `Badge` for shopping cart, and removed
    `Container` from `Toolbar`.

--- ShopSphereUI/src/components/Header.tsx
+++ ShopSphereUI/src/components/Header.tsx
@@ -1,12 +1,45 @@
-import { AppBar, Container, Toolbar, Typography } from "@mui/material"
+import { ShoppingCart } from "@mui/icons-material";
+import { AppBar, Badge, Box, Button, Container, IconButton, List, ListItem, Stack, Toolbar, Typography } from "@mui/material"
+import { NavLink } from "react-router";
+
+const links = [
+  { title: "Home", to: "/" },
+  { title: "Catalog", to: "/catalog" },
+  { title: "About", to: "/about" },
+  { title: "Contact", to: "/contact" },
+]
+
+const navStyles = {
+  color: "inherit",
+  textDecoration: "none",
+  "&:hover": {
+    color: "text.primary"
+  },
+  "&.active": {
+    color: "warning.main"
+  }
+}
 
 export default function Header(props:any) {
     return (
       <AppBar position="static" sx={{mb:4}}>
-        <Toolbar>
-          <Container>
-           <Typography variant="h6">ShopSphere</Typography>
-          </Container>
+        <Toolbar sx={{display:"flex", justifyContent:"space-between"}}>
+          <Box sx={{display:"flex", alignItems:"center"}}> 
+              <Typography variant="h6">ShopSphere</Typography>
+            <Stack direction="row">
+              { links.map(link => 
+              <Button component={NavLink} sx={navStyles} to={link.to}>{link.title}</Button>  
+           ) }
+           </Stack>
+           </Box>
+
+           <Box sx={{display:"flex", alignItems:"center"}}> 
+            <IconButton size="large" edge="start" color="inherit">
+              <Badge badgeContent={2} color="secondary">
+                <ShoppingCart />
+                </Badge>
+            </IconButton>
+           </Box>
         </Toolbar>
 </AppBar>);
   }
