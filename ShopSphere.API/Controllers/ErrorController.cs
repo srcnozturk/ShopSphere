40fde29bfeb66653e1a21022f6750f9a016092d0
srcnozturk <sercan_ozturk59@hotmail.com>
2025-03-31 16:58:20 +0300

    Add ErrorController to handle various HTTP errors
    Introduced a new `ErrorController` class in the `ShopSphere.API.Controllers` namespace. This controller is decorated with `[Route("api/[controller]")]` and `[ApiController]` attributes. It includes action methods to handle different types of HTTP errors:
    - `NotFoundError` returns a 404 Not Found response.
    - `BadRequestError` returns a 400 Bad Request response.
    - `UnauthorizedError` returns a 401 Unauthorized response.
    - `ServerError` throws an exception to simulate a server error.
    - `ValidationError` adds model errors to the `ModelState` and returns a 400 Validation Problem response.

---
+++ ShopSphere.API/Controllers/ErrorController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShopSphere.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ErrorController : ControllerBase
+    {
+        [HttpGet("not-found")]
+        public IActionResult NotFoundError()
+        {
+            return NotFound(); //404
+        }
+        [HttpGet("bad-request")]
+        public IActionResult BadRequestError()
+        {
+            return BadRequest(); //400
+        }
+        [HttpGet("unauthorized")]
+        public IActionResult UnauthorizedError()
+        {
+            return Unauthorized(); //401
+        }
+        [HttpGet("server-error")]
+        public IActionResult ServerError()
+        {
+            throw new Exception("server-error"); //401
+        }
+        [HttpGet("validation-error")]
+        public IActionResult ValidationError()
+        {
+            ModelState.AddModelError("validation-error", "This is a validation error");
+            ModelState.AddModelError("validation-error2", "This is a validation error2");
+            return ValidationProblem(); //400
+        }
+    }
+}
