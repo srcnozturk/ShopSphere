ffa923c59a69309cbe77ebe8870a7c7074791941
srcnozturk <sercan_ozturk59@hotmail.com>
2025-04-14 19:02:30 +0300

    Add Counter component with Redux integration and MUI styling
    This commit introduces a new `Counter` component that integrates with the Redux store to manage and display a counter value. It uses Material-UI components for styling and layout. The component provides buttons to increment, decrement, and increment the counter by a specified amount, enhancing the user interface with interactive and visually appealing elements.

---
+++ ShopSphereUI/src/pages/counter/Counter.tsx
@@ -0,0 +1,18 @@
+import { Button, ButtonGroup, Typography } from "@mui/material";
+import { decrement, increment, incrementByAmount, useAppDispatch, useAppSelector } from "./counterSlice";
+
+export default function Counter() {
+    const count =useAppSelector((state) => state.counter.value) // Accessing the counter value from the Redux store)
+    const dispatch = useAppDispatch(); // Getting the dispatch function from the Redux store
+
+    return (
+        <>
+        <Typography>{count}</Typography>
+       <ButtonGroup>
+        <Button onClick={()=> dispatch(increment())}>increment</Button>
+        <Button onClick={()=> dispatch(decrement())}>decrement</Button>
+        <Button onClick={()=> dispatch(incrementByAmount(5))}>incrementByAmount</Button>
+        </ButtonGroup>
+        </>
+    );
+}
