9bd7513bbc5274f0345377f508a9dccb850a8452
srcnozturk <sercan_ozturk59@hotmail.com>
2025-04-14 17:28:12 +0300

    Add Redux Toolkit and react-redux; configure Redux store
    Added @reduxjs/toolkit and react-redux dependencies to package.json and package-lock.json. Updated main.tsx to include the Redux Provider component, wrapping the application with the Redux store. Created store.ts to configure the Redux store using configureStore from @reduxjs/toolkit. Defined RootState and AppDispatch types in store.ts for use with the Redux store.

--- ShopSphereUI/src/main.tsx
+++ ShopSphereUI/src/main.tsx
@@ -4,11 +4,15 @@ import './index.css'
 import { RouterProvider } from 'react-router'
 import { router } from './router/Routes.tsx'
 import {  CartConextProvider } from './context/CartContext.tsx'
+import { store } from './store/store.ts'
+import { Provider } from 'react-redux'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Provider store={store}>
     <CartConextProvider> 
     <RouterProvider router={router} />
     </CartConextProvider>
+    </Provider>
   </StrictMode>,
 )
