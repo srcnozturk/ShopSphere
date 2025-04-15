1175c5455c18968deda84d0abe7d0ad079c9d7dd
srcnozturk <sercan_ozturk59@hotmail.com>
2025-04-15 22:35:55 +0300

    Add async thunks for cart actions and update state handling
    - Updated import to include `createAsyncThunk` from `@reduxjs/toolkit`.
    - Added import for `requests` from `../../api/requests`.
    - Updated `CartState` interface to include `status` property.
    - Initialized `status` in `initialState` with value `"idle"`.
    - Created `addItemToCart` and `deleteItemFromCart` async thunks.
    - Updated `cartSlice` with `extraReducers` for thunk actions.
    - Exported `setCart` action for use in components.

--- ShopSphereUI/src/pages/cart/cartSlice.ts
+++ ShopSphereUI/src/pages/cart/cartSlice.ts
@@ -1,13 +1,39 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { Cart } from "../../model/ICart";
+import requests from "../../api/requests";
 
 interface CartState {
     cart: Cart |null;
+    status:string;
 }
 
 const initialState: CartState = {
     cart: null,
+    status: "idle",
 };
+
+export const addItemToCart= createAsyncThunk<Cart,{productId:string,quantity:number}>(
+    "cart/addItemToCart",
+    async ({productId, quantity=1}) => {
+        try
+        {
+            return await requests.Cart.addItem(productId, quantity);
+        }
+        catch (error) {
+            console.log("Error adding item to cart:", error);
+        }
+    });
+
+export const deleteItemFromCart= createAsyncThunk<Cart,{productId:string,quantity:number}>(
+    "cart/deleteItemFromCart",
+    async ({productId, quantity=1}) => {
+        try{
+            return await requests.Cart.deleteItem(productId, quantity);
+        }
+        catch (error) {
+            console.log("Error deleting item to cart:", error);
+        }
+    });
 
 export const cartSlice = createSlice({
     name: "cart",
@@ -16,7 +42,33 @@ export const cartSlice = createSlice({
         setCart: (state, action) => {
             state.cart = action.payload;
         },
+    },
+    extraReducers: (builder) => {
+        builder.addCase(addItemToCart.pending, (state,action) => {
+            console.log(action);
+            state.status = "pending";
+        });
+        builder.addCase(addItemToCart.fulfilled, (state, action) => {
+            state.cart = action.payload;
+            state.status = "idle";
+        });
+        builder.addCase(addItemToCart.rejected, (state) => {
+            state.status = "idle";
+        });
+        builder.addCase(deleteItemFromCart.pending, (state,action) => {
+            console.log(action);
+            state.status = "pending";
+        });
+        builder.addCase(deleteItemFromCart.fulfilled, (state, action) => {
+            state.cart = action.payload;
+            state.status = "idle";
+        });
+        builder.addCase(deleteItemFromCart.rejected, (state) => {
+            state.status = "idle";
+        });
     }
 });
+    
+      
 
 export const { setCart } = cartSlice.actions; // Exporting the actions for use in components
